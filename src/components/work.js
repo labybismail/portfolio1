bffe8ec9a1ff338bf6316d0b29b414c12b450906
Ismail LABYB <labybismail2002@gmail.com>
2023-01-20 21:09:21 +0100

    re-order cards

--- src/components/work.js
+++ src/components/work.js
@@ -4,7 +4,7 @@ export default function Work(){
     const card1Style = {backgroundImage: `url(${card1Img})`};
     const card2Style = {backgroundImage: `url(${card1Img})`};
     const card3Style = {backgroundImage: `url(${card3Img})`};
-    const pStyle={bottom:"0",right:"5px", color:"whitesmoke",position:"absolute",boxShadow:"2px 2px 10px gold",borderRadius:"9px",backgroundColor:"white",color:'black'}
+    const pStyle={bottom:"0",right:"5px",position:"absolute",boxShadow:"2px 2px 10px gold",borderRadius:"9px",backgroundColor:"white",color:'black'}
     return(
         <div className="col-10" style={{backgroundColor:"rgb(0,0,20)",height:"100vh"}}>
             <div className="col-12 d-flex " style={{marginTop: "70px"}}>
@@ -15,16 +15,16 @@ export default function Work(){
                         </div>
                     </a>
                 </div>
-                <div className="col-4 offset-2 ">
+                {/* <div className="col-4 offset-2 ">
                     <a href="https://imlteamfit.000webhostapp.com" >
                         <div className="m-2 cardsStyle" style={card2Style}>
                             <p className="fs-5" style={pStyle}>imlteamfit</p>
                     </div>
                     </a>
-                </div>
+                </div> */}
             </div>
             <div className="col-12">
-                <div className="col-4 offset-1 mx-auto " style={{height: "30%",marginTop: "50px"}} >
+                <div className="col-4 offset-7  " style={{height: "30%",marginTop: "50px"}} >
                     <a href="https://labybportfolio.netlify.app" >
                         <div className="m-2 cardsStyle" style={card3Style}>
                             <p className="fs-5" style={pStyle}>portfolio</p>
