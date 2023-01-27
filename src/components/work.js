577f7bbe670a0fd895d3a347a10559ebe3fa9837
Ismail LABYB <labybismail2002@gmail.com>
2023-01-27 22:35:19 +0100

    add 4th work

--- src/components/work.js
+++ src/components/work.js
@@ -1,10 +1,12 @@
 import card1Img from './img/card1img.png';
 import card2Img from './img/card2Img.jpeg';
 import card3Img from './img/card3Img.png';
+import card4Img from './img/card4Img.png';
 export default function Work(){
     const card1Style = {backgroundImage: `url(${card1Img})`};
     const card2Style = {backgroundImage: `url(${card2Img})`};
     const card3Style = {backgroundImage: `url(${card3Img})`};
+    const card4Style = {backgroundImage: `url(${card4Img})`};
     const pStyle={bottom:"0",right:"5px",position:"absolute",boxShadow:"2px 2px 10px gold",borderRadius:"9px",backgroundColor:"white",color:'black'}
     return(
         <div className="col-10" style={{backgroundColor:"rgb(0,0,20)",height:"100vh"}}>
@@ -24,11 +26,18 @@ export default function Work(){
                     </a>
                 </div>
             </div>
-            <div className="col-12">
-                <div className="col-4 offset-4  " style={{height: "30%",marginTop: "50px"}} >
+            <div className="col-12 d-flex">
+                <div className="col-4 offset-1  " style={{height: "30%",marginTop: "50px"}} >
                     <a href="https://labybportfolio.netlify.app" >
                         <div className="m-2 cardsStyle" style={card3Style}>
                             <p className="fs-5" style={pStyle}>portfolio</p>
+                        </div>
+                    </a>
+                </div>
+                <div className="col-4 offset-2  " style={{height: "30%",marginTop: "50px"}} >
+                    <a href="https://game1-rock.netlify.app" >
+                        <div className="m-2 cardsStyle" style={card4Style}>
+                            <p className="fs-5" style={pStyle}>rock-paper-scissor</p>
                         </div>
                     </a>
                 </div>
