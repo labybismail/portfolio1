e59d784859a9e4e79ec6ab14855a6e605b954e22
Ismail LABYB <labybismail2002@gmail.com>
2023-01-29 13:45:32 +0100

    edits on contact vue

--- src/components/contact.js
+++ src/components/contact.js
@@ -4,35 +4,33 @@ import googleIcon from './img/google.svg'
 import linkedinIcon from './img/linkedin.svg'
 import phoneIcon from './img/phone-solid.svg'
 export default function Contact(){
-    const styleImg={width:"50px",height:"50px"}
-    const div1Style={right: "5px",top:"220px"}
-    const div2Style={top: "310px"}
-    const div3Style={top: "400px"}
-    const div4Style={top: "480px"}
+    const imgStyle ={borderRadius:'50%',backgroundColor:'white',padding:'5px'}
+
     return(
-        <>
-        <div className="col-12 col-xl-10  position-relative div1Contact">
-            <p class="pStyle" style={{top:"235px"}} >labybismail.dev@gmail.com</p>
-            <div class="col-2 divsStyle " style={div1Style}>
-                <img src={googleIcon} style={styleImg}  alt="mail" />
+        
+        <div className="col-12 col-xl-10 d-flex align-items-center justify-content-center flex-column div1Contact">
+            <div className="col-12 d-flex justify-content-center text-light ">
+                <img src={googleIcon} style={imgStyle} width="50px" height="50px" alt="mail" />
+                <p className="m-3" >labybismail.dev@gmail.com</p>
             </div>
             
-            <p class="pStyle" style={{top:"324px"}}>https://github.com/labybismail/portfolio1</p>
-            <div class="col-2 divsStyle" style={div2Style}>
-                <img src={githubIcon}  style={styleImg}  alt="github" />
+             
+            <div className="col-12 d-flex justify-content-center text-light" >
+                <img src={githubIcon}  width="50px" height="50px" style={imgStyle}  alt="github" />
+                <p className="m-3" >https://github.com/labybismail/portfolio1</p>
             </div>
-            <p class="pStyle" style={{top:"415px"}}>https://www.linkedin.com/in/labybismail</p>
-            <div class="col-2 divsStyle" style={div3Style}>
-                <img src={linkedinIcon}  style={styleImg}  alt="linkedin" />
+            
+            <div className="col-12 d-flex justify-content-center text-light" >
+                <img src={linkedinIcon}  style={imgStyle}  width="50px" height="50px"  alt="linkedin" />
+                <p className="m-3" >https://www.linkedin.com/in/labybismail</p>
             </div>
-            <p class="pStyle" style={{top:"495px"}}>+212 681 33 23 02</p>
-            <div class="col-2 divsStyle" style={div4Style}>
-                <img src={phoneIcon}  style={styleImg}  alt="phone" />
-            </div>
-            <div className="col-7 mx-auto fs-4 text-light float-right mt-5" >
-                Get in touch with us to get the ball rolling
-            </div>    
+            
+            <div className="col-12 d-flex justify-content-center text-light" >
+                <img src={phoneIcon}  style={imgStyle}  width="50px" height="50px"  alt="phone" />
+                <p className="m-3">+212 681 33 23 02</p>
+            </div> 
+            
         </div>
-        </>
+        
     )
 }
